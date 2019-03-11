911827d14d66c0667a64c1379445b7814bfcc337
kiovchev <iov4ev_kokki@abv.bg>
2019-03-11 14:21:24 +0200

    Added classes Order and OrderDetail

---
+++ BiesPro_System/BiesPro.Models/Order.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace BiesPro.Models
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+
+        //nav prop
+        //TODO Create property in oposite class Relation one-to one
+        public int VendorId { get; set; }
+        public ClientOrVendor Vendor { get; set; }
+
+        //TODO Create property in oposite class Relation one-to one
+        public int ClientId { get; set; }
+        public ClientOrVendor Client { get; set; }
+
+        public DateTime StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+
+        //TODO Create property in oposite class Relation one-to one
+        public int OrderDetailId { get; set; }
+        public OrderDetail OrderDetails { get; set; }
+
+
+    }
+}
